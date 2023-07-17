918fdc64877108fd2ef4baf79a27e6685dee7756
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-17 20:31:48 +0000

    feat: to exe with docker and optimize memory

--- amass.py
+++ amass.py
@@ -2,12 +2,37 @@
 
 import os
 import sys
+from typing import Generator
+import time
 
-domain = sys.argv[1]
+def amass(domain: str, use_docker: bool = True) -> Generator[str, None, None]:
+    print("[+] amass")
+    if use_docker:
+        amass_command = f'docker run -v OUTPUT_DIR_PATH:/.config/amass/ caffix/amass enum -passive -norecursive -noalts -d {domain} 2>/dev/null'
 
-def amass():
-    amass_command = f'amass enum -passive -norecursive -noalts -d {domain} 2>/dev/null | anew amass.subs'
-    os.system(amass_command)
+    else:
+        amass_command = f'amass enum -passive -norecursive -noalts -d {domain} 2>/dev/null'
 
-amass()
+        
+    for line in os.popen(amass_command):
+        yield line.strip()
+        
+       
+domain = 'caterpillar.com'
+amass(domain)
+
+
+# TODO: benchmark execution with and without docker using below sample
+
+# start_time = time.time()
+# for url in amass(domain):
+#     continue
+# end_time = time.time()
+# execution_time = end_time - start_time
+# print(f'execution time with yield: {execution_time}')
+
+
+
+
+
 
