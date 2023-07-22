efbbb21e5c4bd2efc44abbfe72c64c5cb4420322
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-22 23:33:45 +0000

    refactor(sub_finder): main.py to include functions

--- sub_finder/main.py
+++ sub_finder/main.py
@@ -2,19 +2,54 @@
 import os
 from multiprocessing import Pool
 from typing import Generator
-from sources.findomain import *
-from sources.crtsh import *
-from sources.amass import *
-from sources.subfinder import *
-from sources.assetfinder import *
-from sources.crtsh_curl import *
+from configs import DOMAIN
 
-def main() -> Generator[str, None, None]:
+
+def amass(use_docker: bool = True) -> Generator[str, None, None]:
+    print("[+] amass")
+    if use_docker:
+        amass_command = f'docker run -v OUTPUT_DIR_PATH:/.config/amass/ caffix/amass enum -passive -norecursive -noalts -d {DOMAIN} 2>/dev/null'
+
+    else:
+        amass_command = f'amass enum -passive -norecursive -noalts -d {DOMAIN} 2>/dev/null'
+
+        
+    for line in os.popen(amass_command):
+        yield line.strip()
+    
+    
+def findomain(use_docker: bool = True) -> Generator[str, None, None]:
+    print("[+] findomain")
+    if use_docker:
+        findomain_command = f'docker run -it edu4rdshl/findomain:latest -t {DOMAIN} -q 2>/dev/null'
+    else: 
+        findomain_command = f'findomain -t {DOMAIN} -q 2>/dev/null'
+    for line in os.popen(findomain_command):
+        yield line.strip()
+        
+        
+def subfinder(use_docker: bool = True) -> Generator[str, None, None]:
+    if use_docker:
+        subfinder_command = f"docker run projectdiscovery/subfinder:latest -d {DOMAIN} -all"
+    else:
+        subfinder_command = f"subfinder -d {DOMAIN} -all"  
+        
+    for line in os.popen(subfinder_command):
+        yield line.strip()
+    
+        
+def crtsh_curl() -> Generator[str, None, None]:
+    command = f'curl -sk "https://crt.sh/?q=%.{DOMAIN}&output=json" | tr "," "\\n" | awk -F\'"\' \'/name_value/ {{gsub(/\\*\\./, "", $4); gsub(/\\\\n/,"\\n",$4);print $4}}\''
+    for line in os.popen(command):
+        yield line.strip()
+
+
+def get_subs() -> Generator[str, None, None]:
     pool = Pool()
 
     seen_results = set()
     
-    sub_finders = [findomain, crtsh, amass, subfinder, assetfinder, crtsh_curl]
+    sub_finders = [findomain, amass, subfinder, crtsh_curl]
     
     for result in pool.imap(lambda f: list(f()), sub_finders):
         yield from (res for res in result if res not in seen_results and not seen_results.add(res))
@@ -24,5 +59,6 @@ def main() -> Generator[str, None, None]:
     
 
 
-for line in main():
+if __name__ == '__main__'
+for line in get_subs():
     print(line)
