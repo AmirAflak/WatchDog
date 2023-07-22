76314e2aaaa9341948ec0ef281e19a2afb71eed6
amiraflak <mehrdad.aflakparast@gmail.com>
2023-07-22 23:20:59 +0000

    feat: to run with docker

--- sub_finder/sources/subfinder.py
+++ sub_finder/sources/subfinder.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-
 import os
 from typing import Generator
 from configs import DOMAIN
 
-def subfinder():
-    subfinder_command = f"subfinder -d {DOMAIN} -all"
+def subfinder(use_docker: bool = True) -> Generator[str, None, None]:
+    if use_docker:
+        subfinder_command = f"docker run projectdiscovery/subfinder:latest -d {DOMAIN} -all"
+    else:
+        subfinder_command = f"subfinder -d {DOMAIN} -all"  
+        
     for line in os.popen(subfinder_command):
         yield line.strip()
         
     
-# for line in subfinder():
-#     print(line)
+for line in subfinder():
+    print(line)
 
 __all__ = ['subfinder']
