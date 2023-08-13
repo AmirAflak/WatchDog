ba1dd64a19325eb177b0fc134342f994102ea7c7
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-13 22:19:33 +0000

    feat: to include datetime field at insertion time

--- mongodb.py
+++ mongodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from configs import MONGO_HOST, MONGO_PORT, MONGO_DB_NAME
+from datetime import datetime 
 
 def get_client(host, port, db_name):
     return MongoClient(host, port)[db_name]
@@ -8,5 +9,5 @@ def close_client(client):
     client.close()
 
 def store_message(message, session, collection_name):
-    session[collection_name].insert_one({'subdomain': message})
+    session[collection_name].insert_one({'subdomain': message, 'fetched_time': datetime.now()})
     
