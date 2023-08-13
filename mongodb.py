6cbf51ecc46e6550a0834912f19400f12690445a
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-13 22:29:05 +0000

    style(store_message): minor change

--- mongodb.py
+++ mongodb.py
@@ -8,6 +8,5 @@ def get_client(host, port, db_name):
 def close_client(client):
     client.close()
 
-def store_message(message, session, collection_name):
+def store_message(session, message, collection_name):
     session[collection_name].insert_one({'subdomain': message, 'fetched_time': datetime.now()})
-    
