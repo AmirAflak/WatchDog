b82de16a479e577d97e4f9242f3fdc14b59ed31f
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-13 22:15:58 +0000

    add: mongodb.py to interact with mongodb

---
+++ mongodb.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from configs import MONGO_HOST, MONGO_PORT, MONGO_DB_NAME
+
+def get_client(host, port, db_name):
+    return MongoClient(host, port)[db_name]
+
+def close_client(client):
+    client.close()
+
+def store_message(message, session, collection_name):
+    session[collection_name].insert_one({'subdomain': message})
+    
