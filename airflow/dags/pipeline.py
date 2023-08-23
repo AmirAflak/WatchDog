f06b463aa08b44bdb7ae51f41f9d1bccf43f4efb
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-23 21:52:46 +0000

    add: airflow dags with astronomer config

---
+++ airflow/dags/pipeline.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+
+from datetime import datetime, timedelta
+
+
+with DAG(
+    dag_id=f"subdomain_finder_flow",
+    start_date=datetime(2023,7,28),
+    schedule=timedelta(minutes=10),
+    catchup=False,
+    tags= ["kafka", "spark", "mongoDB"],
+    default_args={
+        "retries": 2,
+        "retry_delay": timedelta(minutes=3)
+    }
+) as dag:
+    t0 = DummyOperator(
+        task_id='start_pipeline',
+    )
+    
+    t1 = BashOperator(
+        task_id='kafka_producer',
+        bash_command='/home/hyper/scripts/airflow_bash/run_producer.sh ',
+    )
+    
+    t2 = BashOperator(
+        task_id='kafka_consumer',
+        depends_on_past=False,
+        bash_command='/home/hyper/scripts/airflow_bash/run_cassandra.sh ',
+    )
+
