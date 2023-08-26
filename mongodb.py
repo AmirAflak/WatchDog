42812b9441c78b19cb541200bf38ab4c5bfbbb01
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-26 22:10:30 +0000

    fix: embedded db_name into client connection

--- mongodb.py
+++ mongodb.py
@@ -4,8 +4,7 @@ from datetime import datetime
 
 class MongoDBClient:
     def __init__(self, host, port, db_name, username, password):
-        self.client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/")
-        self.db = self.client[db_name]
+        self.client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/")[db_name]
     
     def store_message(self, message, collection_name):
         self.client[collection_name].insert_one(message)
