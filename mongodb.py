e2e4fcc1ceb90c7ea970f239dd25b4868bd68494
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-26 21:27:09 +0000

    remove: old structure

--- mongodb.py
+++ mongodb.py
@@ -2,15 +2,6 @@ from pymongo import MongoClient
 from configs import MONGO_HOST, MONGO_PORT, MONGO_DB_NAME
 from datetime import datetime 
 
-# def get_client(host, port, db_name, username, password):
-#     return MongoClient(f"mongodb://{username}:{password}@{host}:{port}/")[db_name]
-
-# def close_client(client):
-#     client.close()
-
-# def store_message(session, message, collection_name):
-#     session[collection_name].insert_one(message)
-
 class MongoDBClient:
     def __init__(self, host, port, db_name, username, password):
         self.client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/")
