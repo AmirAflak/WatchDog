63832f2d3592fc09382fc0b9d13fabdcd217cbf1
amiraflak <mehrdad.aflakparast@gmail.com>
2023-08-28 19:29:16 +0000

    remove: consumer taks

--- airflow/dags/pipeline.py
+++ airflow/dags/pipeline.py
@@ -1,11 +1,22 @@
+import os          
+import sys 
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 
-
 from datetime import datetime, timedelta
 
+# from mongodb_consumer import JsonConsumer
+# from producer import JsonProducer
+
+
+# producer_path = os.path.abspath('/home/mehrdad/watchdog/producer.py')
+# sys.path.append(producer_path)
+
+# consumer_path = os.path.abspath('/home/mehrdad/watchdog/mongodb_consumer.py')
+# sys.path.append(consumer_path)
 
 with DAG(
     dag_id=f"subdomain_finder_flow",
@@ -24,12 +35,7 @@ with DAG(
     
     t1 = BashOperator(
         task_id='kafka_producer',
-        bash_command='/home/hyper/scripts/airflow_bash/run_producer.sh ',
+        bash_command='/home/mehrdad/watchdog/airflow/dags/run_producer.sh ',
     )
     
-    t2 = BashOperator(
-        task_id='kafka_consumer',
-        depends_on_past=False,
-        bash_command='/home/hyper/scripts/airflow_bash/run_cassandra.sh ',
-    )
-
+    t0 >> t1
