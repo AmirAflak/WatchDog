cd1f8c60bbfc28076eb34d8a27d6ece396388de5
NimaFaghih <nimafaghih.a@gmail.com>
2023-09-11 16:40:41 +0000

    Add telegram notification program

---
+++ notification/telegram.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+apiURL= f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+def new_subdomain(new_sub):
+    msg = f"[+] new subdomain: {new_sub}"
+    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+
+def new_asset(new_ass):
+    msg = f"[+] new Asset: {new_ass}"
+    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+
+def new_service(new_serv):
+    msg = f"[+] New service: {new_serv}"
+    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+
+def latest_asset(latest_ass):
+    msg = f"[+] Latest asset: {latest_ass}"
+    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+
+def Change_Service(change_serv):
+    msg = f"[+] Change Servicee: {change_serv}"
+    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text':msg})
