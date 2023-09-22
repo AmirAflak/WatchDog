938cdecb97f3ca53f10e95a9dee9486c9b9fa2a3
Amirreza Aflakparast <84932095+AmirAflak@users.noreply.github.com>
2023-09-22 22:54:34 +0330

    add: chat_id env variable

--- utils/notification/telegram.py
+++ utils/notification/telegram.py
@@ -5,24 +5,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 apiURL= f'https://api.telegram.org/bot{TOKEN}/sendMessage'
 
 def new_subdomain(new_sub):
     msg = f"[+] new subdomain: {new_sub}"
-    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+    response = requests.post(apiURL, json={'chat_id': CHAT_ID, 'text': msg})
 
 def new_asset(new_ass):
     msg = f"[+] new Asset: {new_ass}"
-    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+    response = requests.post(apiURL, json={'chat_id': CHAT_ID, 'text': msg})
 
 def new_service(new_serv):
     msg = f"[+] New service: {new_serv}"
-    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+    response = requests.post(apiURL, json={'chat_id': CHAT_ID, 'text': msg})
 
 def latest_asset(latest_ass):
     msg = f"[+] Latest asset: {latest_ass}"
-    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text': msg})
+    response = requests.post(apiURL, json={'chat_id': CHAT_ID, 'text': msg})
 
 def Change_Service(change_serv):
     msg = f"[+] Change Service: {change_serv}"
-    response = requests.post(apiURL, json={'chat_id': -1001612767860, 'text':msg})
+    response = requests.post(apiURL, json={'chat_id': CHAT_ID, 'text':msg})
